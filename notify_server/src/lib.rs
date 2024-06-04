d62d83f726752ba6cdcaf13e5d7a80e32734ec44
wangxiaochuang <932698529@qq.com>
2024-06-05 07:54:53 +0800

    feature: refactor code to workspace and add notify server

---
+++ notify_server/src/lib.rs
@@ -0,0 +1,19 @@
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use sse::sse_handler;
+mod sse;
+
+const INDEX_HTML: &str = include_str!("../index.html");
+
+pub fn get_router() -> Router {
+    Router::new()
+        .route("/events", get(sse_handler))
+        .route("/", get(index_handler))
+}
+
+async fn index_handler() -> impl IntoResponse {
+    Html(INDEX_HTML)
+}
--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use chat::{config::AppConfig, get_router};
+use notify_server::get_router;
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
@@ -9,10 +9,9 @@ async fn main() -> Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let config = AppConfig::load()?;
-    let addr = format!("0.0.0.0:{}", config.server.port);
+    let addr = "0.0.0.0:6687";
 
-    let app = get_router(config);
+    let app = get_router();
     let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
 
