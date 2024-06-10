93b23991280882aaa83bfc162d2af4e4e1ecae38
wangxiaochuang <932698529@qq.com>
2024-06-10 18:56:49 +0800

    feature: learn how to test middleware

--- chat_server/src/middlewares/auth.rs
+++ chat_server/src/middlewares/auth.rs
@@ -39,3 +39,53 @@ pub async fn verify_token(State(state): State<AppState>, req: Request, next: Nex
         };
     next.run(req).await
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{config::AppConfig, User};
+
+    use super::*;
+    use anyhow::Result;
+    use axum::{body::Body, http::Request, middleware::from_fn_with_state, routing::get, Router};
+    use tower::ServiceExt;
+
+    async fn handler() -> String {
+        "hello".to_string()
+    }
+
+    #[tokio::test]
+    async fn verify_token_middleware_should_work() -> Result<()> {
+        let config = AppConfig::load()?;
+        let (state, _tdb) = AppState::try_test_new(config).await?;
+
+        let user = User::new(1, "jack", "jack@admin");
+        let token = state.ek.sign(user)?;
+
+        let app = Router::new()
+            .route("/", get(handler))
+            .layer(from_fn_with_state(state.clone(), verify_token))
+            .with_state(state);
+
+        let req = Request::builder()
+            .uri("/")
+            .header("Authorization", format!("Bearer {}", token))
+            .body(Body::empty())?;
+        let res = app.clone().oneshot(req).await?;
+        assert_eq!(res.status(), StatusCode::OK);
+
+        // no token
+        let req = Request::builder().uri("/").body(Body::empty())?;
+        let res = app.clone().oneshot(req).await?;
+        assert_eq!(res.status(), StatusCode::UNAUTHORIZED);
+
+        // bad token
+        let req = Request::builder()
+            .uri("/")
+            .header("Authorization", "Bearer bad-token")
+            .body(Body::empty())?;
+        let res = app.oneshot(req).await?;
+        assert_eq!(res.status(), StatusCode::FORBIDDEN);
+
+        Ok(())
+    }
+}
